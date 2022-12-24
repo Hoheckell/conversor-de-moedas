7173649e9368e0f4eb5de9da2b8c42e53876430c
Hoheckell <hoheckel.filho@prideglobal.com>
2022-12-24 18:14:18 -0300

    setup and user module begining

---
+++ src/modules/user/services/user.service.ts
@@ -0,0 +1,35 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities';
+import { UserDto } from '../dto/user.dto';
+import { UserUpdateDto } from '../dto/update-user.dto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(userdto: UserDto): Promise<User> {
+    const user = await this.userRepository.create(userdto);
+    return await this.userRepository.save(user);
+  }
+
+  async show(userId: number): Promise<User> {
+    return await this.userRepository.findOne({ where: { id: userId } });
+  }
+
+  async list(): Promise<Array<User>> {
+    return await this.userRepository.find();
+  }
+
+  async update(id: number, userdto: UserUpdateDto): Promise<void> {
+    await this.userRepository.update({ id }, userdto);
+  }
+
+  async delete(userId: number): Promise<void> {
+    await this.userRepository.delete(userId);
+  }
+}
