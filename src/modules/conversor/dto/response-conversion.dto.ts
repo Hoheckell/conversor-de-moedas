4877cfcef6b2cb3c8326ee7c751164a97b708521
Hoheckell <hoheckel.filho@prideglobal.com>
2022-12-28 11:51:41 -0300

    documentation update

--- src/modules/conversor/dto/response-conversion.dto.ts
+++ src/modules/conversor/dto/response-conversion.dto.ts
@@ -1,10 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 export class ResponseConversionDto {
+  @ApiProperty({
+    description: 'Returned id of the register',
+    type: 'number',
+  })
+  @IsNumber()
   id: number;
+  @ApiProperty({
+    description: 'Returned User id that requested',
+    type: 'number',
+  })
+  @IsNumber()
   userId: number;
+
+  @ApiProperty({
+    description: 'Currency BRL | USD | EUR | JPY',
+    type: 'string',
+  })
+  @IsString()
   currencyFrom: string;
+
+  @ApiProperty({
+    description: 'Original amount for conversion',
+    type: 'number',
+  })
+  @IsNumber()
   currencyFromValue: number;
+
+  @ApiProperty({
+    description: 'Currency BRL | USD | EUR | JPY',
+    type: 'string',
+  })
+  @IsString()
   currencyTo: string;
-  quote: number;
-  timestamp: Date;
-  result: number;
+
+  @ApiProperty({
+    description: 'Returned value on destination currency',
+    type: 'number',
+  })
+  @IsNumber()
+  quote?: number;
+
+  @ApiProperty({ description: 'Returned date of the query', type: 'date' })
+  timestamp?: string;
 }
